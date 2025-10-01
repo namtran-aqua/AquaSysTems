188904ae4805fef7c39e602ed3965c81c8bdc78a
TRAN KIET (TRẦN KIỆT) (Comp_HEV) <kiet.trantuan@aquavietnam.vn>
2025-10-01 13:26:09 +0700

    demo

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,93 @@
-﻿using AquaSolution.Data.Connection;
+﻿//using AquaSolution.Data.Connection;
+//using AquaSolution.Data.Data;
+//using AquaSolution.Server;
+//using AquaSolution.Server.SignalR;
+//using AquaSolution.Shared.Permissions;
+//using Microsoft.AspNetCore.Authentication.JwtBearer;
+//using Microsoft.AspNetCore.Identity;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.IdentityModel.Tokens;
+//using System.Text;
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+
+//builder.Services.AddControllersWithViews();
+//builder.Services.AddRazorPages();
+////-----------------------CustomConfig---------------------------------
+//builder.Services.AddDbContext<AquaDbContext>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+//builder.Services.AddControllers();
+//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//    .AddJwtBearer(options =>
+//    {
+//        options.TokenValidationParameters = new TokenValidationParameters
+//        {
+//            ValidateIssuer = false,
+//            ValidateAudience = false,
+//            ValidateLifetime = true,
+//            ValidateIssuerSigningKey = true,
+//            IssuerSigningKey = new SymmetricSecurityKey(
+//                Encoding.UTF8.GetBytes("VerySecretKey12345"))
+//        };
+
+//        // Optional: để thấy lỗi nếu token invalid
+//        options.Events = new JwtBearerEvents
+//        {
+//            OnAuthenticationFailed = context =>
+//            {
+//                Console.WriteLine($"Authentication failed: {context.Exception.Message}");
+//                return Task.CompletedTask;
+//            }
+//        };
+//    });
+//builder.Services.AddAppServices();
+//builder.Services.AddAuthorization();
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+//builder.Services.AddSignalR();
+////--------------------------------------------------------------------
+//var app = builder.Build();
+//app.UsePathBase("/AquaSolution");
+//using (var scope = app.Services.CreateScope())
+//{
+//    var db = scope.ServiceProvider.GetRequiredService<AquaDbContext>();
+//    db.Database.Migrate();
+//    //DbSeeder.SeedData(db);
+//}
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseWebAssemblyDebugging();
+//}
+//else
+//{
+//    app.UseExceptionHandler("/Error");
+//    app.UseHsts();
+//}
+//app.UseSwagger();
+//app.UseSwaggerUI(c =>
+//{
+//    c.SwaggerEndpoint("v1/swagger.json", "AquaSolution API v1");
+//    c.RoutePrefix = "swagger";
+//});
+
+//app.UseAuthentication(); 
+//app.UseAuthorization();  
+//app.UseHttpsRedirection();
+
+//app.UseBlazorFrameworkFiles();
+//app.UseStaticFiles();
+
+//app.UseRouting();
+
+//app.MapRazorPages();
+//app.MapControllers();
+//app.MapHub<SignalrHub>("/signalrhub");
+//app.MapFallbackToFile("index.html");
+
+//app.Run();
+using AquaSolution.Data.Connection;
 using AquaSolution.Data.Data;
 using AquaSolution.Server;
 using AquaSolution.Server.SignalR;
@@ -59,21 +148,17 @@ using (var scope = app.Services.CreateScope())
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 else
 {
     app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("v1/swagger.json", "AquaSolution API v1");
-    c.RoutePrefix = "swagger";
-});
-
-app.UseAuthentication(); 
-app.UseAuthorization();  
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseHttpsRedirection();
 
 app.UseBlazorFrameworkFiles();
